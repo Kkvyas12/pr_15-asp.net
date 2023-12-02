1af1bdc746c57b3a7bedbdcc254af3234d336b69
Krishna Vyas <vyaskrishnak123456789@gmail.com>
2023-12-02 22:18:15 +0530

    Add project files.

---
+++ state/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="state.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+        <div>
+            <asp:Label ID="lblCounter" runat="server" Text="Counter: "></asp:Label>
+            <asp:Label ID="lblCounterValue" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Button ID="btnIncrement" runat="server" Text="Increment Counter" OnClick="btnIncrement_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ state/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace state
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Initialize the counter when the page is loaded for the first time
+                Session["Counter"] = 0;
+            }
+
+            // Update the label text with the current counter value
+            lblCounterValue.Text = Session["Counter"].ToString();
+        }
+
+        protected void btnIncrement_Click(object sender, EventArgs e)
+        {
+            // Increment the counter when the button is clicked and update Session state
+            int counter = Convert.ToInt32(Session["Counter"]);
+            counter++;
+            Session["Counter"] = counter;
+
+            // Update the label text with the updated counter value
+            lblCounterValue.Text = Session["Counter"].ToString();
+        }
+    }
+}
